20eec85b2525b9bc20e26e8e56a103a3b2d7ba74
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-25 12:37:52 +0530

    Popping and finding Peek element using Stack Operations

--- Program.cs
+++ Program.cs
@@ -5,20 +5,30 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks and Queue Program");
-
             Console.WriteLine("Creating Stack using Linked List");
 
-            CreateStack stack = new CreateStack();
+            LinkedListStack stack = new LinkedListStack();
             stack.Push(70);
             stack.Push(30);
             stack.Push(56);
             stack.Display();
 
+            Console.WriteLine("\n-----------------\n");
+
+            stack.Peek();
+            stack.Pop();
+            stack.IsEmpty();
+            stack.Display();
         }
     }
 
 
+
 }
+    
+
+
+
 
     
 
