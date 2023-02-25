7b7cf5c8ab14b7db0f829d0ec1ecb525c0d1fb77
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-25 11:46:02 +0530

    Popping up elements using stack operations

--- Program.cs
+++ Program.cs
@@ -5,6 +5,23 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks and Queue Program");
+
+            Console.WriteLine("Creating Stack using Linked List");
+
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+
+            Console.WriteLine("\n-----------------\n");
+
+            stack.Peek();
+            stack.Pop();
+            stack.IsEmpty();
+            stack.Display();
         }
     }
 }
+    
+
