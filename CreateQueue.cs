2b8be48ab2fec97b38914da11df7841227f7de1b
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-25 13:07:04 +0530

    Created queue and enqueue Elements

---
+++ CreateQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StacksAndQueues
+{
+    public class CreateQueue
+    {
+        
+            public Node head = null;
+
+            public void Enqueue(int data)
+            {
+                Node node = new Node(data);
+
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    Node temp = head;
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+
+
+                }
+                Console.WriteLine("{0} inserted into Queue", node.data);
+            }
+
+            public void Display()
+            {
+                Node temp = this.head;
+                if (temp == null)
+                {
+                    Console.WriteLine("Queue is empty");
+                    return;
+                }
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@
         {
             Console.WriteLine("Welcome to Stacks and Queue Program");
 
+            Console.WriteLine("Creating Queue using Linked List");
+
+            CreateQueue queue = new CreateQueue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+
         }
     }
 
