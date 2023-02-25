6a037a4bd843fdfd30dfda6385c0e27aa0290bb3
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-25 11:39:01 +0530

    Create Stack and push elements

---
+++ CreateStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StacksAndQueues
+{
+    public class CreateStack
+    {
+      
+            private Node top;
+            public void Push(int value)
+            {
+                Node node = new Node(value);
+                if (this.top == null)
+                    node.next = null;
+                else
+                    node.next = this.top;
+                this.top = node;
+                Console.WriteLine("{0} pushed to stack ", value);
+            }
+            public void Display()
+            {
+                Node temp = this.top;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+
--- Program.cs
+++ Program.cs
@@ -5,6 +5,19 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks and Queue Program");
+
+            Console.WriteLine("Creating Stack using Linked List");
+
+            CreateStack stack = new CreateStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+
+
         }
+
     }
 }
+    
+
