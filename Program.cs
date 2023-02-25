2d9a89e96a3ca1ef61949b4277c2c1b99db2b9fc
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-25 12:27:50 +0530

    Greeting Message

--- Program.cs
+++ Program.cs
@@ -6,22 +6,9 @@
         {
             Console.WriteLine("Welcome to Stacks and Queue Program");
 
-            Console.WriteLine("Creating Stack using Linked List");
-
-            LinkedListStack stack = new LinkedListStack();
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
-            stack.Display();
-
-            Console.WriteLine("\n-----------------\n");
-
-            stack.Peek();
-            stack.Pop();
-            stack.IsEmpty();
-            stack.Display();
         }
     }
+
 }
     
 
